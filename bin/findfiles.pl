87a0ed16402c1cd2717957458043461509819def
Derek Carter <goozbach@friocorte.com>
2009-04-06 14:39:12 -0400

    added sample3 as an override example, modified findfiles to print each meta.ini, cleaned sample/meta.ini

--- bin/findfiles.pl
+++ bin/findfiles.pl
@@ -4,6 +4,8 @@ use warnings;
 
 use File::Find::Rule;
 use File::Slurp;
+use Config::Auto;
+use Data::Dumper;
 
 # bring in the stoplist 
 my @stoplist = read_file('.stoplist');
@@ -15,10 +17,35 @@ my @subdirs = File::Find::Rule->directory()
                               ->maxdepth(1)
                               ->not(File::Find::Rule->name(@stoplist))
                               ->in( '.' );
+my $url_base = 'http://blog.friocorte.com/base/';
+my $override = Config::Auto::parse('.master.ini');
 
+sub check_exists {
+  my ($top, $var, $config, $dir) = @_;
+  if ($$config{$top}{$var}) {
+    print "proj_$var:\t" . $$config{$top}{$var} . "\n" 
+  } else {
+    if ($var eq 'url') {
+      print "proj_$var:\t" . $$override{$top}{$var} . $dir . "\n";
+    } else {
+      print "proj_$var:\t" . $$override{$top}{$var} . "\n";
+    }
+  }
+}
+
+foreach my $itr (@subdirs) {
+  print "dir_name:\t$itr\n";
+  my $config = Config::Auto::parse("$itr/meta.ini") || print "no meta file\n";
+
+  check_exists('main', 'name', $config, $itr);
+  check_exists('main', 'url', $config, $itr);
+  check_exists('main', 'blurb', $config, $itr);
+  print "proj_author:\t" . $$config{'main'}{'author'} . ": <" . $$config{'main'}{'auth_email'} . ">\n";
+  print "--------------\n";
+}
 
 #debug code to test file finding
-print join("\n", @subdirs) . "\n";
-print "--------------\n";
+#print join("\n", @subdirs) . "\n";
+
 print join(", ", @stoplist) . "\n";
 
