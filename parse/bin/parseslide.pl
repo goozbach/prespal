1af1641e790bded2e2f2ebc9c3ebf241d30af0c9
Derek Carter <goozbach@friocorte.com>
2009-04-17 15:27:49 -0400

    fixing timespace for temp stuff

--- parse/bin/parseslide.pl
+++ parse/bin/parseslide.pl
@@ -1,61 +1,63 @@
 #!/usr/bin/perl 
-#===============================================================================
-#
-#         FILE:  parseslide.pl
-#
-#        USAGE:  ./parseslide.pl 
-#
-#  DESCRIPTION: Parses the slideshow format for prespal
-#
-#      OPTIONS:  ---
-# REQUIREMENTS:  ---
-#         BUGS:  ---
-#        NOTES:  ---
-#       AUTHOR:   (), <>
-#      COMPANY:  
-#      VERSION:  1.0
-#      CREATED:  04/13/2009 05:56:04 PM EDT
-#     REVISION:  ---
-#===============================================================================
-
 use strict;
 use warnings;
-
 use Parse::RecDescent;
 use Data::Dumper;
 
-#my $grammar = q{
-  #slide: "^\.slide" slideContent
-  #slideContent: "^  "(\S.*|slideNote|slideHandout)
-  #slideNote: "  \.note\n" slideNoteContent
-  #slideHandout}; 
-
 my $testslide = <<EOD;
 .slide
+  # start slide 1
   # What is "prespal"?
-  * It's a simple markdown based syntax to   slideshow/handout/outline presentation mangement tool
+  * It's a simple markdown based syntax to slideshow/handout/outline presentation mangement tool
   * Create one simple text file and it generates different output formats
   * The slideshow is done by a forked version of S5
   * Eventually we'll have output formats for pdf, and others
-
   .note
     * You can also have notes which only the presenter can see
+    .putting .slide inside
     * Keen.
-
   .handout
     There is nothing in this section of the handout
+  # end slide 1
+
+.slide
+  # slide 2
+  # What is "prespal"?
+  * It's a simple markdown based syntax to slideshow/handout/outline presentation mangement tool
+  * Create one simple text file and it generates different output formats
+  * The slideshow is done by a forked version of S5
+  .putting .slide inside
+  * Eventually we'll have output formats for pdf, and others
+  .note
+    * You can also have notes which only the presenter can see
+    .putting .slide inside
+    * Keen.
+  .handout
+  .putting .slide inside
+    There is nothing in this section of the handout
+  # end slide 2
 
 EOD
 
-my $grammar = q{
-  slideContent: startSlide /.*/s
 
-  startSlide: /\.slide/
- 
-  note: /\.note/
-  };
+$Parse::RecDescent::skip = '';
+my $grammar = q{
+Presentation: Slides /\Z/
+Slides: Slide(s?)
+Slide: Content Notes Handouts
+Content: Slide_Marker L1indents
+Slide_Marker: /\.slide/
+L1indents: L1indent(s?)
+L1indent: /  \w.*/
+Notes: Note(s?)
+Note: Note_Marker L2indents
+Note_Marker: /  \.note/
+L2indents: L2indent(s?)
+L2indent: /    \w.*/
+Handouts: Handout(s?)
+Handout: Handout_Marker L2indents
+Handout_Marker: /  \.handout/
+};
 
 my $parser = Parse::RecDescent->new($grammar);
-
-print Dumper $parser->slideContent($testslide);
-print Dumper $parser->startSlide($testslide);
+print Dumper $parser->Presentation($testslide);
