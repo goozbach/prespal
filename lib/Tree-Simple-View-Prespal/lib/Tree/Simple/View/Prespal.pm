f7e6df2caae85174c7e430175a84bd0fad97da22
Derek Carter <goozbach@friocorte.com>
2009-05-01 12:55:40 -0400

    new library for prespal

---
+++ lib/Tree-Simple-View-Prespal/lib/Tree/Simple/View/Prespal.pm
@@ -0,0 +1,70 @@
+package Tree::Simple::View::Prespal;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Tree::Simple::View::Prespal - A Tree::Simple::View class for the Prespal format.
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Used for parsing of prespal content
+
+    use Tree::Simple::View::Prespal;
+
+    my $foo = Tree::Simple::View::Prespal->new();
+    ...
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Derek Carter, C<< <derek at friocorte.com> >>
+
+=head1 BUGS
+
+Please email me any bug reports
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Tree::Simple::View::Prespal
+
+
+=head1 ACKNOWLEDGEMENTS
+
+Tree::Simple::View module author
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Derek Carter, all rights reserved.
+
+=cut
+
+1; # End of Tree::Simple::View::Prespal
---
+++ lib/Tree-Simple-View-Prespal/t/00-load.t
@@ -0,0 +1,9 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+	use_ok( 'Tree::Simple::View::Prespal' );
+}
+
+diag( "Testing Tree::Simple::View::Prespal $Tree::Simple::View::Prespal::VERSION, Perl $], $^X" );
